ce687a82f8d60bcd4f1e9f49373653d11cbd1e83
Umer Faisal <umerfaisal1@icloud.com>
2025-12-06 20:27:46 +0500

    added structs

--- main.cpp
+++ main.cpp
@@ -1,13 +1,57 @@
-//Ride Sharing Project
-//Usman Joined
+// Ride Sharing Project
+// Usman Joined
 
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout<<"Hello world"<<endl;
-	
+struct Place;
+struct RoadLink
+{
+	Place *to;
+	int cost;
+	RoadLink *next;
+};
+struct Place
+{
+	char *name;
+	RoadLink *firstLink;
+	Place *next;
+};
+Place *placeHead = nullptr;
+struct User
+{
+	int userId;
+	char *name;
+	int isDriver; // 1=driver,0=passenger
+	int rating;
+	User *left; // BST
+	User *right;
+};
+struct RideOffer
+{
+	int offerId;
+	int driverId;
+	Place *startPlace;
+	Place *endPlace;
+	int departTime;
+	int capacity;
+	int seatsLeft;
+	RideOffer *next;
+};
+struct RideRequest
+{
+	int requestId;
+	int passengerId;
+	Place *fromPlace;
+	Place *toPlace;
+	int earliest;
+	int latest;
+	RideRequest *next;
+};
+
+int main()
+{
+	cout << "Hello world" << endl;
 
 	return 0;
-
 }
